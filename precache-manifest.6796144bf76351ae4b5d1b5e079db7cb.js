7ea4699523ed4f628ac1f78b08c4ee4177304153
jaykhandelwal525 <jaykhandelwal525@gmail.com>
2021-06-21 19:44:27 +0530

    d

---
+++ precache-manifest.6796144bf76351ae4b5d1b5e079db7cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a988a2400ccdd7c360dbfd75fca6bc78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcb8b76921650d4031fc",
+    "url": "/static/css/main.e3341884.chunk.css"
+  },
+  {
+    "revision": "73f4c04d6d58ae4d91f2",
+    "url": "/static/js/2.14393964.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.14393964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb8b76921650d4031fc",
+    "url": "/static/js/main.d18ff175.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
